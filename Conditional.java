9d4767f9aa7e93c7fc38f74cac451ba47e3428ef
Valentine Onyekachi <onyekachi_val@yahoo.com>
2025-12-11 16:10:22 +0200

    testing simple conditional statements logic

---
+++ Conditional.java
@@ -0,0 +1,14 @@
+public class Conditional {
+    public static void main(String[] args) {
+        //conditional statements
+        int x = 20;
+        int y = 35;
+
+        if (x>10 && y>30){
+            System.out.println("Moi");
+            System.out.println("Tervetolua");
+        }
+        else
+            System.out.println("Bye");
+    }
+}
