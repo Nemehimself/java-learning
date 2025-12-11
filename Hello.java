cb7d26b83259da6bd03c068e44664f8e2f75d4aa
Valentine Onyekachi <onyekachi_val@yahoo.com>
2025-12-11 12:55:11 +0200

    added more java Variables

--- Hello.java
+++ Hello.java
@@ -1,11 +1,21 @@
 public class Hello {
     public static void main(String[] args) {
-        
+        //variables
         int num1 = 5;
         int num2 = 5;
         int result = num1 + num2;
 
-        System.out.println("Hello World!");
+        String greet = "Hello World!";
+        double  PI = 3.127;
+        boolean inActive = true;
+        float half = 0.5f;
+        char password = '@';
+
+        System.out.println(greet);
         System.out.println(result);
+        System.out.println(PI);
+        System.out.println(inActive);
+        System.out.println(half);
+        System.out.println(password);
     }
 }
