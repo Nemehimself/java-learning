03d8a6ec9d6c8d34902308980345deae564ef220
Valentine Onyekachi <onyekachi_val@yahoo.com>
2025-12-11 13:54:23 +0200

    Java Operators

---
+++ Operators.java
@@ -0,0 +1,18 @@
+public class Operators {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 2;
+
+        int add = num1 + num2; // addition
+        int sub = num1 - num2; // subtraction
+        int mul = num1 * num2; // multiplication
+        int div = num1 / num2; // division
+        int rem = num1 % num2; // remainder
+
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(rem);
+    }
+}
