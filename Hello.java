474b007324268d75af6e50546e72cf94b590dccd
Valentine Onyekachi <onyekachi_val@yahoo.com>
2025-12-11 11:42:50 +0200

    Day 1 progress: Hello World and Variables

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args) {
+        
+        int num1 = 5;
+        int num2 = 5;
+        int result = num1 + num2;
+
+        System.out.println("Hello World!");
+        System.out.println(result);
+    }
+}
