d129799dc92d79f6879cacdc196951ab4c22b948
Valentine Onyekachi <onyekachi_val@yahoo.com>
2025-12-12 12:22:13 +0200

    ternary operations

--- Conditional.java
+++ Conditional.java
@@ -3,12 +3,23 @@ public class Conditional {
         //conditional statements
         int x = 20;
         int y = 35;
+        int z = 40;
 
-        if (x>10 && y>30){
+        if (x>y && x>z){
             System.out.println("Moi");
             System.out.println("Tervetolua");
         }
-        else
-            System.out.println("Bye");
+        else if (y>x && y>z)
+            System.out.println("Mitä kuuluu");
+        else 
+            System.out.println("moi moi");
+
+        //ternary operators
+        int n = 5;
+        int result = 0;
+
+        result = n % 2 == 0 ? 10 : 20;
+
+        System.out.println(result);
     }
 }
