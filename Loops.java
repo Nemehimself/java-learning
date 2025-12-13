b9d2525673db4677c7f5ce0fdffd76871794e79a
Valentine Onyekachi <onyekachi_val@yahoo.com>
2025-12-13 11:58:52 +0200

    While loop

---
+++ Loops.java
@@ -0,0 +1,14 @@
+public class Loops {
+    public static void main(String[] args) {
+        //Loops - while, For, Do while
+
+        int i = 1;
+        
+        while(i<=4) 
+            {
+            System.out.println("Hi " + i);
+            i++;
+        }
+        System.out.println( "Bye " + i);
+    }
+}
