9016a43b286725b7d8933cd854d97bdad5aa7491
Valentine Onyekachi <onyekachi_val@yahoo.com>
2025-12-13 12:01:01 +0200

    nested While loop

--- Loops.java
+++ Loops.java
@@ -7,6 +7,12 @@ public class Loops {
         while(i<=4) 
             {
             System.out.println("Hi " + i);
+
+            int j = 1;
+            while (j<=3) {
+                System.out.println("Hello " + j);
+                j++;
+            }
             i++;
         }
         System.out.println( "Bye " + i);
