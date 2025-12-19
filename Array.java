7cfbc6a2b81c9ba6ba8df3436e9c26b05fa9beff
Valentine Onyekachi <onyekachi_val@yahoo.com>
2025-12-20 00:17:48 +0200

    learning arrays in java

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args) {
+        int nums [] = {3, 7, 2, 4};
+
+        System.out.println(nums[1]);
+
+        for(int i=0; i<4; i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
